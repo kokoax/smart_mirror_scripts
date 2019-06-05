00044ee43e6ff8f208abed276841732b78fa08d5
kokoax <yomo4808@gmail.com>
2019-06-05 21:38:04 +0900

    最初に表示するとこけしちゃった

--- public/js/scheduler.js
+++ public/js/scheduler.js
@@ -6,6 +6,10 @@ function getMinute(min) {
   return min * 1000000;
 }
 
+weather()
+todo()
+time()
+
 setInterval(weather, getMinute(60));
 setInterval(todo, getMinute(60));
 setInterval(time, getSecond(30));
